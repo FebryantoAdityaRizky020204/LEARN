50a97e34cfef0c1c420cad0fed0a6cd4e67db5c3
ADITYA_RIZKY_F <febryantoadityarizky@gmail.com>
2024-09-06 22:40:12 +0700

    P1: 06092024-2239

--- CODEPOLITAN-DEVHANDAL/javascript-restfull-express-js/rest/index.js
+++ CODEPOLITAN-DEVHANDAL/javascript-restfull-express-js/rest/index.js
@@ -2,12 +2,21 @@ const express = require('express')
 
 const app = express();
 
+
+/**
+ * ? kita harus mengaktifkan middleware dibawah agar express 
+ * ? bisa membaca request body berupa form-endoded maupun json
+ */
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/order', (request, response) => {
     response.send('GET order response')
 })
 
 app.post('/order', (request, response) => {
-    response.send('POST order response')
+    const {item} = request.body
+    response.send(`POST order response, Item : ${item}`)
 })
 
 app.listen(8080, () => {
